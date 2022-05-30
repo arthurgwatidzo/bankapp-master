d3446d468b34fbd20d0b8263eb703ccc27e2bdbe
Owen2024 <owen@africasoftwarearchitects.com>
2022-05-30 11:02:25 +0200

    implement frontend and swagger configs

--- src/main/java/za/co/bank/atm/app/application/controller/AccountWebController.java
+++ src/main/java/za/co/bank/atm/app/application/controller/AccountWebController.java
@@ -1,2 +1,36 @@
-package za.co.bank.atm.app.application.controller;public class AccountWebController {
+package za.co.bank.atm.app.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import za.co.bank.atm.app.application.service.AccountService;
+import za.co.bank.atm.app.domain.dto.AccountDto;
+import za.co.bank.atm.app.domain.dto.CurrencyAccountDto;
+import za.co.bank.atm.app.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+/**
+ * @author Arthur Gwatidzo email:arthur.gwatidzo@gmail.com
+ */
+
+@Controller
+public class AccountWebController {
+
+    private AccountService accountService;
+
+    public AccountWebController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping(path = "/")
+    public String displayAccountBalances(Model model) throws ResourceNotFoundException {
+
+        List<AccountDto> accounts = accountService.getAccountBalances(1, true);
+        List<CurrencyAccountDto> currencyAccounts = accountService.getCurrencyAccounts(1, true);
+        model.addAttribute("accounts", accounts);
+        model.addAttribute("currencyAccounts", currencyAccounts);
+
+        return "accountbalances";
+    }
 }
--- src/main/java/za/co/bank/atm/app/configuration/SwaggerConfiguration.java
+++ src/main/java/za/co/bank/atm/app/configuration/SwaggerConfiguration.java
@@ -2,13 +2,14 @@ package za.co.bank.atm.app.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.*;
-import springfox.documentation.service.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * @author Arthur Gwatidzo email:arthur.gwatidzo@gmail.com
@@ -22,7 +23,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("za.co.bank.atm.app.application.controller"))
-                .paths(regex("/clients"))
+                .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false)
                 .apiInfo(metaData());
